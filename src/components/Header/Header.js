536bfd6b1856ebb2de7a505bd38b946b3d903afc
Jacob Walton <jawalton6616@gmail.com>
2021-02-15 12:59:39 -0600

    created header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">Movies and TV 🎞</header>
+    </div>
+  );
+};
+
+export default Header;
