baf577768ec68259207b19a221fbba208080e302
Jacob Walton <jawalton6616@gmail.com>
2021-02-16 05:36:17 -0600

    creating search page

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,15 +1,45 @@
-import { TextField } from "@material-ui/core";
+import {
+  TextField,
+  createMuiTheme,
+  ThemeProvider,
+  Tabs,
+  Button,
+} from "@material-ui/core";
+import { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import "./search.css";
+
 const Search = () => {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
   const [type, setType] = useState(0);
 
+  const setSearchText = (e) => {
+    console.log(e);
+  };
   return (
     <div>
-      <TextField
-        style={{ flex: 1 }}
-        label="Search"
-        className="search"
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ display: "flex", margin: "15px 0" }}>
+          <TextField
+            style={{ flex: 1 }}
+            label="Search"
+            className="searchBox"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button variant="contained" style={{ marginLeft: 10 }}>
+            <SearchIcon />
+          </Button>
+        </div>
+        <Tabs value={type}></Tabs>
+      </ThemeProvider>
     </div>
   );
 };
