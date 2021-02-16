e8540f8287559a6da0403e4f5ea34088a29cceac
Jacob Walton <jawalton6616@gmail.com>
2021-02-16 05:50:11 -0600

    added api call for search page

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -3,13 +3,20 @@ import {
   createMuiTheme,
   ThemeProvider,
   Tabs,
+  Tab,
   Button,
 } from "@material-ui/core";
 import { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import "./search.css";
+import axios from "axios";
 
 const Search = () => {
+  const [numOfPages, setNumOfPages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [content, setContent] = useState();
+
   const darkTheme = createMuiTheme({
     palette: {
       type: "dark",
@@ -19,11 +26,22 @@ const Search = () => {
     },
   });
 
+  const getSearch = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+          process.env.REACT_APP_API_KEY
+        }&llanguage=en-US&query=${searchText}&page=${page}&include_adult=false`
+      );
+      setContent(data.results);
+      setNumOfPages(data.total_pages);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const [type, setType] = useState(0);
 
-  const setSearchText = (e) => {
-    console.log(e);
-  };
   return (
     <div>
       <ThemeProvider theme={darkTheme}>
@@ -38,7 +56,18 @@ const Search = () => {
             <SearchIcon />
           </Button>
         </div>
-        <Tabs value={type}></Tabs>
+        <Tabs
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(e, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+        >
+          <Tab style={{ width: "50%" }} label="Search for Movies"></Tab>
+          <Tab style={{ width: "50%" }} label="Search for TV Series"></Tab>
+        </Tabs>
       </ThemeProvider>
     </div>
   );
